42842e6d820e4c010157906a84f72206a7d7ed21
pomiano <miloszpomian@gmail.com>
2026-03-07 18:21:33 +0100

    dodanie komunikatow do antidotum

--- antidote/src/anti_main.c
+++ antidote/src/anti_main.c
@@ -10,23 +10,59 @@ int main(int argc, char **argv) {
 
     //checking original loader
     EFI_HANDLE partitionHandleOriginal = findPartition(BackupLoaderW);
-    if(partitionHandleOriginal == NULL) return 1;
+    if(partitionHandleOriginal == NULL) {
+        printf("CRITICAL ERROR: Backup loader not found\n");
+        printf("Press any key...\n");
+        getchar();
+        return 1;
+    }
 
     //delete fake loader
-    EFI_HANDLE partitionHandleFake = findPartition(BackupLoaderW);
+    EFI_HANDLE partitionHandleFake = findPartition(OriginalLoaderW);
     if(partitionHandleFake != NULL){
         EFI_FILE_PROTOCOL* root = openRoot(partitionHandleFake);
-        if(root == NULL) return 1;
-        status = delete_file(root,OriginalLoaderW);
+        if(root != NULL) {
+            status = delete_file(root,OriginalLoaderW);
+            if(EFI_ERROR(status) && status != EFI_NOT_FOUND){
+                printf("ERROR: Could not delete fake loader\n");
+                printf("INFO: Attempting to rename the file to a fallback name");
+                printf("Press any key...\n");
+                getchar();
+                status = renameFile(root, OriginalLoaderW, FallbackLoaderW);
+                if(!EFI_ERROR(status)){
+                    printf("SUCCESS: The name of the snak gate has been changed\n");
+                    printf("Press any key...\n");
+                    getchar();
+                }else{
+                    printf("ERROR: Could not rename fake loader\n");
+                    printf("CRITICAL ERROR: System restoration failed\n");
+                    printf("Press any key...\n");
+                    getchar();
+                    return 1;
+                }
+            } else {
+                printf("SUCCESS: The snake gate has been removed\n");
+                printf("Press any key...\n");
+                getchar();
+            }
+        }
     }
 
     //renaming original loader
-    EFI_FILE_PROTOCOL* root = openRoot(partitionHandleOriginal);
-    if(root == NULL) return 1;
+    EFI_FILE_PROTOCOL* rootOriginal = openRoot(partitionHandleOriginal);
+    if(rootOriginal == NULL) return 1;
 
-    status = renameFile(root, BackupLoaderW, OriginalLoaderW);
-    if(EFI_ERROR(status)) return 1;
+    status = renameFile(rootOriginal, BackupLoaderW, OriginalLoaderW);
+    if(EFI_ERROR(status)) {
+        printf("CRITICAL ERROR: Failed to rename original file\n");
+        printf("Press any key...\n");
+        getchar();
+        return 1;
+    }
 
+    printf("SUCCES: System restored succesfully!\n");
+    printf("Press any key...\n");
+    getchar();
 
     return 0;
 
--- common/include/config.h
+++ common/include/config.h
@@ -8,10 +8,11 @@
 // ASCII paths for POSIX-like functions (e.g., fopen)
 static const char* const OriginalLoaderA = "\\EFI\\Microsoft\\Boot\\bootmgfw.efi";
 static const char* const BackupLoaderA   = "\\EFI\\Microsoft\\Boot\\bootmgfw_ms.efi";
+static const char* const FallbackLoaderA   = "\\EFI\\Microsoft\\Boot\\snake_gate.efi";
 
 // Wide character paths for native UEFI functions (e.g., Delete, Rename) 
 static const CHAR16* const OriginalLoaderW = (const CHAR16*)L"\\EFI\\Microsoft\\Boot\\bootmgfw.efi";
 static const CHAR16* const BackupLoaderW   = (const CHAR16*)L"\\EFI\\Microsoft\\Boot\\bootmgfw_ms.efi";
-
+static const CHAR16* const FallbackLoaderW  = (const CHAR16*)L"\\EFI\\Microsoft\\Boot\\snake_gate.efi";
 
 #endif
